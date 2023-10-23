85d944dc6a74dcbebf8f0eb1fefc48e7da1ae45e
Yuhan Lin <yuhanlin@udel.edu>
2023-10-22 21:25:36 -0400

    I hope it works.

--- src/quizzer/QuizEdit.tsx
+++ src/quizzer/QuizEdit.tsx
@@ -12,37 +12,43 @@ export const QuizEdit = ({
     deleteQuiz,
     switchEdit,
     resetView
-}: {) => {
+}: {
+    quiz: Quiz;
+    editQuiz:(id:number, newQuiz: Quiz)=>void;
+    deleteQuiz: (id:number)=>void;
+    switchEdit:()=>void;
+    resetView: ()=>void;
+}) => {
     const [newQuiz, setNewQuiz] = useState<Quiz>({ ...quiz });
 
-    const editQuestion = (questionId: number, newQuestion: Question) => {
+    const editQuestion = (questionId: number, newQuestion: Question):void => {
         setNewQuiz({
             ...newQuiz,
-            questionList: newQuiz.questionList.map(
+            questionList: newQuiz.questionList.map((question: Question):Question => question.id === questionId ? newQuestion : question
             )
         });
     };
 
-    const removeQuestion = (questionId: number) => {
+    const removeQuestion = (questionId: number):void => {
         setNewQuiz({
             ...newQuiz,
-            questionList: newQuiz.questionList.filter(
+            questionList: newQuiz.questionList.filter((question: Question):boolean => !(question.id === questionId)
             )
         });
     };
 
-    const saveChanges = () => {
+    const saveChanges = ():void => {
         editQuiz(quiz.id, { ...newQuiz });
     };
 
-    const swapQuestion = (idx1: number, idx2: number) => {
+    const swapQuestion = (idx1: number, idx2: number):void => {
         setNewQuiz({
             ...newQuiz,
             questionList: newQuiz.questionList.map(
                 (q: Question, idx: number): Question => {
                     if (idx === idx1) return newQuiz.questionList[idx2];
                     if (idx === idx2) return newQuiz.questionList[idx1];
-                    return;
+                    return q;
                 }
             )
         });
@@ -79,7 +85,7 @@ export const QuizEdit = ({
                             ) => {
                                 setNewQuiz({
                                     ...newQuiz,
-                                    published: 
+                                    published: !newQuiz.published
                                 });
                             }}
                         ></Form.Check>
